d12e8b25f44535b61d494df47368b1f9a6ee8de1
Ayman-deve <53484122+Ayman-deve@users.noreply.github.com>
2022-03-03 15:58:03 +0200

    .
    .

--- src/app/models/priority.ts
+++ src/app/models/priority.ts
@@ -2,12 +2,14 @@ export class Priority {
     priority_ID: string;
     priority_name: string;
     description: string;
-
+    priority_img: string;
     constructor(priority){
         {
             this.priority_ID = priority.prioirity_ID || "";
             this.priority_name = priority.priority_name || "";
             this.description = priority.descriprtion || "";
+            this.priority_img = priority.priority_img || "";
+
         }
     }
 }
