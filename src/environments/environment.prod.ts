8b5925ae1fa07506feb1134d10ec325b08f451a8
Ssoliman2018 <38067419+Ssoliman2018@users.noreply.github.com>
2022-03-13 21:08:39 +0200

    .
    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBBjQJUHIgN3XYY-kBCzOO8CbFYVFhlHIM",
+    authDomain: "helm-8734b.firebaseapp.com",
+    databaseURL: "https://helm-8734b.firebaseio.com",
+    projectId: "helm-8734b",
+    storageBucket: "helm-8734b.appspot.com",
+    messagingSenderId: "758507429266",
+    appId: "1:758507429266:web:7485783bbc27dfb96cf54b",
+    measurementId: "G-KE7BHEEYVM"
+  }
 };
